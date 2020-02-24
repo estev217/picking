a9fdfb6802bb5c53a7cf36bf552b2dc2dd8dbdb1
estevao <estevao.fernandes@cordongroup.com>
2020-02-24 16:49:28 +0100

    bootstrap works

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,12 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+// any CSS you import will output into a single scss file (app.scss in this case)
+import '../scss/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-const $ = require('jquery');
+// const $ = require('jquery');
 require('bootstrap');
