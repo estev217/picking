616ba4dcde965abf3ecba583f7e314d077c9992c
estev217 <estevao.fernandes@live.fr>
2020-03-27 15:14:46 +0100

    user creation is done

--- src/Form/OperateurType.php
+++ src/Form/OperateurType.php
@@ -7,6 +7,7 @@ use App\Entity\Role;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,16 +18,24 @@ class OperateurType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, [
-                "label" => 'Nom d\'utilisateur',
-
+                "label" => false,
             ])
             ->add('role', EntityType::class, [
                 "class" => Role::class,
                 "label" => false,
                 "choice_label" => 'name',
             ])
-            ->add('password', PasswordType::class, [
-                "label" => 'Mot de passe'
+            ->add('password', RepeatedType::class, [
+                "type" => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe doivent être identiques',
+                'first_name' => 'Mot_de_passe',
+                'second_name' => 'Confirmation',
+                'options' => [
+                    'attr' => [
+                        'class' => 'password-field',
+                    ]
+                ],
+                'required' => true,
             ])
         ;
     }
